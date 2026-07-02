c0023fef94a42ea30a87935fca773d5e071f5855
Raphael <grumbachraphael@gmail.com>
2026-07-03 00:36:35 +0300

    installed termusic

--- .config/ghostty/kill.sh
+++ .config/ghostty/kill.sh
@@ -13,5 +13,8 @@
 # @raycast.description kill tmux server and quit ghostty
 # @raycast.author RaphaelGrumbach
 
+# i dont want to see any output
+# i still see A Error: Process terminated with status 1 and i dont want to see it its because of termusic 
 tmux kill-server 2>/dev/null
 osascript -e 'tell application "Ghostty" to quit'
+killall termusic terminal-server >/dev/null 2>&1 || true
