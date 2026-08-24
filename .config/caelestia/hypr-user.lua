95a5f4663790389599189909a47dde09b45c1149
Raphael Grumbach <grumbachraphael@gmail.com>
2026-08-24 17:20:31 +0300

    added SUPER + D shourtcat

--- .config/caelestia/hypr-user.lua
+++ .config/caelestia/hypr-user.lua
@@ -1,3 +1,5 @@
 -- Universal copy/paste, works in terminals and GUI apps alike
 hl.bind("SUPER + C", hl.dsp.send_shortcut({ mods = "CTRL", key = "Insert" }), { description = "Universal copy" })
 hl.bind("SUPER + V", hl.dsp.send_shortcut({ mods = "SHIFT", key = "Insert" }), { description = "Universal paste" })
+hl.unbind("SUPER + D")
+hl.bind("SUPER + D", hl.dsp.exec_cmd("caelestia shell drawers toggle dashboard"), { description = "opens dashboard" })
